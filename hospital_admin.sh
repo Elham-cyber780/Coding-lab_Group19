6061c2bf4692ae4cfd343f078c67f5f78859e00c
Elham-cyber780 <your-github-e.abdulrahm@alustudent.com>
2026-05-30 13:20:59 -0700

    Add initialize_system function

---
+++ hospital_admin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+initialize_system() {
+    if [ ! -d "active_logs" ]; then
+        echo "Creating active_logs directory..."
+        mkdir active_logs
+    fi
+    if [ ! -d "archived_logs" ]; then
+        echo "Creating archived_logs directory..."
+        mkdir archived_logs
+    fi
+    if [ ! -d "reports" ]; then
+        echo "Creating reports directory..."
+        mkdir reports
+    fi
+}
+
+secure_data() {
+    chmod 600 active_logs
+    echo "Permissions set. Here are the current permissions:"
+    ls -l | grep active_logs
+}
+
+initialize_system
+secure_data
+echo "System Environment Secured on $(date)"
