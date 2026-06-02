4ebc4e6fc634e1f44686822d7000c16aa2284e07
root <r.touray@alustudent.com>
2026-06-02 03:05:53 -0700

    Add check to ensure reports directory exists

---
+++ hospital_analysis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Member 5 : Clinical Analyst
+process_vitals() {
+	#Ensure reports directory exists
+	[ ! -d reports ] && mkdir reports; > reports/critical_alerts.txt
+        # Heart Rate critical alerts
+        grep "CRITICAL" active_logs/heart_rate.log | awk '{print $1, $2, $3}' >> reports/critical_alerts.txt
+	# Temperature critical alerts
+	grep "CRITICAL" active_logs/temperature.log | awk '{print $1, $2, $3}' >> reports/critical_alerts.txt
+}
