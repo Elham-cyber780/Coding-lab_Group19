ca05b9bfb8ff4751264c75246d35b6db08a5a469
root <root@DESKTOP-31N1PPD.localdomain>
2026-06-05 16:43:24 +0200

    2. Apply awk to extract Timestamp, Device_ID, and Value for alerts

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -2,7 +2,8 @@
 #  Member 5 : Clinical Analyst
 process_vitals() {
 	#Ensure reports directory exists
-	[ ! -d reports ] && mkdir reports; > reports/critical_alerts.txt
+	[ ! -d reports ] && mkdir reports
+	> reports/critical_alerts.txt
         # Heart Rate critical alerts
         grep "CRITICAL" active_logs/heart_rate.log | awk '{print $1, $2, $3}' >> reports/critical_alerts.txt
 	# Temperature critical alerts
